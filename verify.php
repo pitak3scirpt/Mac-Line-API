cb355e61b021c839983476c6d5636f3bf54ed31e
pitak3scirpt <pitak3g.sc@gmail.com>
2016-11-26 04:43:29 +0700

    update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,8 @@
 <?php
-$access_token = 'xxx';
+$proxy = 'usefixie.com:80';
+$proxyauth = 'fixie:ncIwXPyhNi4cEWL@velodrome';
+
+$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
@@ -9,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
