c6d6657981c666e5ee646994f91eaac46e81945c
pitak3scirpt <pitak3g.sc@gmail.com>
2016-12-11 23:38:48 +0700

    update.php

--- bot.php
+++ bot.php
@@ -15,8 +15,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
-			$text = "I love You.";
+			$text = $event['message']['text']."By Pitak";
 			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
