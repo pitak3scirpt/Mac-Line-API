0af71f7b64c984bb41245222069c35828fc71d9e
pitak3scirpt <pitak3g.sc@gmail.com>
2016-12-15 12:11:12 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 echo "Hello LINE BOT <br>";
 echo "Test Tx BK TEXT <br>";
-$handle = fopen("TxBK.txt", "r");
-while (!feof($handle)) {
-    $buffer = fgets($handle, 4096);
-    echo $buffer;
-}
-fclose($handle);
+$buffer = file_get_contents("TxBK.txt");
+echo $buffer;
 ?>
