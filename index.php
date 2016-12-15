aa6d00446a54ab75c49922da2a6839afa986f3e3
pitak3scirpt <pitak3g.sc@gmail.com>
2016-12-15 12:25:46 +0700

    update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo "Hello LINE BOT <br>";
 echo "Test Tx BK TEXT <br>";
 $buffer = file_get_contents("TxBK.txt");
-$buffer = utf8_decode($buffer);
+$buffer = iconv("UTF-8", "ISO-8859-2//TRANSLIT", $buffer), PHP_EOL;
 echo $buffer;
 ?>
