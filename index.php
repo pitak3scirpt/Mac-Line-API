aed572c4216efd167a667c6791c1e33d75db337b
pitak3scirpt <pitak3g.sc@gmail.com>
2016-12-15 12:32:16 +0700

    update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo "Hello LINE BOT <br>";
 echo "ทดสอบ Test Tx BK TEXT <br>";
 $buffer = file_get_contents("TxBK.txt");
-$buffer = iconv("UTF-8", "ISO-8859-2//TRANSLIT", $buffer);
+$buffer = iconv("UTF-8", "ISO-8859-1//TRANSLIT", $buffer);
 echo $buffer;
 ?>
