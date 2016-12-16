eadb33edebd1ad5ba59f3d44260939af2130f209
Jedi Council <pitak3g.sc@gmail.com>
2016-12-16 17:50:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -62,7 +62,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => [$text,"Hello, Message Two"]
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
