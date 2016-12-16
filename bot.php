4df5c07d18fd6f8b1177501729c15ae15fde61c7
Jedi Council <pitak3g.sc@gmail.com>
2016-12-16 17:38:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -63,14 +63,13 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				'text' => $text,
-				'text' => "By Pitak Mahaman"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages]
+				'messages' => [$messages],[$messages]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
