a1387b3c83371c036ea5e445fbf194e0fe03669e
Jedi Council <pitak3g.sc@gmail.com>
2016-12-16 16:23:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -70,6 +70,7 @@ if (!is_null($events['events'])) {
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
