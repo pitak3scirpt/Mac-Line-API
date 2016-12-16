5be447dcc1193867ff7132dc5488cf37ef026e0b
Jedi Council <pitak3g.sc@gmail.com>
2016-12-16 21:51:01 +0700

    Update pushbot.php

--- pushbot.php
+++ pushbot.php
@@ -1,12 +1,10 @@
 <?php
-$proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:ncIwXPyhNi4cEWL';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('token');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('ppp066', $textMessageBuilder);
+$response = $bot->pushMessage('to', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
