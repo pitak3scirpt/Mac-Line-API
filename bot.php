e28de8c0bc76465712b0e88f0a010ef3a314141e
Jedi Council <pitak3g.sc@gmail.com>
2016-12-17 01:03:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -118,6 +118,32 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			
+			// Make Push Messageing
+			$messages = [
+				'type' => 'text',
+				'text' => "Push ข้อความ เรียบร้อย"
+			];
+			$url = 'https://api.line.me/v2/bot/message/push';
+						$data = [
+				'to' => $touserid,
+				'messages' => [$messages]
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 		}
 	}
 }
