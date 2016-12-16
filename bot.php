bc5c9836e36a51e587661fea042784ee57c7ab64
Jedi Council <pitak3g.sc@gmail.com>
2016-12-17 02:23:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -132,7 +132,7 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 			//echo $result . "\r\n";
-
+			$events = json_decode($result, true);
 			// Make Push Messageing
 			$text = $result['displayName'];
 			$messages = [
