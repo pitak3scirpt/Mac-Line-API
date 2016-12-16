74fb07693622a7d6656dabb4fe06edc446937dd2
Jedi Council <pitak3g.sc@gmail.com>
2016-12-17 02:21:58 +0700

    Update userid.php

--- userid.php
+++ userid.php
@@ -2,7 +2,7 @@
 $proxy = 'velodrome.usefixie.com:80';
 $proxyauth = 'fixie:ncIwXPyhNi4cEWL';
 $access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v2/bot/profile/Uf78577e260519b9029ff035a3f056fd4';
+$url = 'https://api.line.me/v2/bot/profile/<userid>';
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
