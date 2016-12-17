72335298749f334bbfb86230bde803f4749e13ea
Jedi Council <pitak3g.sc@gmail.com>
2016-12-17 22:30:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,6 +14,77 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
+		// Reply only when Follow me.
+		if ($event['type'] == 'follow') {
+			$gentext = " ขอบคุณที่ติดตามเรา เราขอตรวจสอบสักครู่ก่อนอนุญาตให้ Bot ตอบสนองคุณ";
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Make a POST Request to Messaging API to reply to follower
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages]
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+			
+			// Find User Data
+			$url = 'https://api.line.me/v2/bot/profile/'.$touserid;
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			//echo $result . "\r\n";
+			$events = json_decode($result, true);
+			// Make Push Messageing
+			$displayName = $events['displayName'];
+			$userId = $events['userId'];
+			$text = $displayName."\n".$userId;
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+				//.'\nRequest '.$reqtext
+			];
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+				'to' => 'Uf95ee3607bc3d6696b2116de202f97d3',
+				'messages' => [$messages]
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+		
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
