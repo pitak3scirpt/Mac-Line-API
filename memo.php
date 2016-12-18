db5a2916c04bada54dd715dd458721d90f28c44e
Jedi Council <pitak3g.sc@gmail.com>
2016-12-18 12:20:13 +0700

    Update memo.php

--- memo.php
+++ memo.php
@@ -3,9 +3,12 @@
 <body>
 
 <?php
-$myfile = fopen("jedicouncil.txt", "r") or die("Unable to open file!");
-echo fgets($myfile);
-fclose($myfile);
+  $myfile = fopen("jedicouncil.txt", "r") or die("Unable to open file!");
+  echo fgets($myfile);
+  fclose($myfile);
+  $myfile = file_get_contents("jedicouncil.txt");
+  echo "\n";
+  echo $myfile;
 ?>
 
 </body>
