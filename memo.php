28eb18b1525ec8c65c33171c5b251c791f3fb483
Jedi Council <pitak3g.sc@gmail.com>
2016-12-18 12:13:22 +0700

    Update memo.php

--- memo.php
+++ memo.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-$myfile = fopen("masteryoda.txt", "r") or die("Unable to open file!");
+$myfile = fopen("jedicouncil.txt", "r") or die("Unable to open file!");
 echo fgets($myfile);
 fclose($myfile);
 ?>
