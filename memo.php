b3f2c7238703f62d7898bfbcc5e96518a91b8b87
Jedi Council <pitak3g.sc@gmail.com>
2016-12-18 12:09:21 +0700

    Update memo.php

--- memo.php
+++ memo.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+$myfile = fopen("masteryoda.txt", "r") or die("Unable to open file!");
 echo fgets($myfile);
 fclose($myfile);
 ?>
