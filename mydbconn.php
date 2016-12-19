785136c4d5babbd797bd5c8b9c0434021a0c9f05
Jedi Council <pitak3g.sc@gmail.com>
2016-12-19 19:43:17 +0700

    Update mydbconn.php

--- mydbconn.php
+++ mydbconn.php
@@ -1,7 +1,7 @@
 <?php
 $username = "x77x84xhahimosz4";
 $password = "zae2msvvkqfuku8x ";
-$hostname = "o61qijqeuqnj9chh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"; 
+$hostname = "mysql://x77x84xhahimosz4:zae2msvvkqfuku8x@o61qijqeuqnj9chh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kqn8nhfuxzaqjkkp"; 
 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) 
