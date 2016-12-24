c414eb491fce86780fc5121d7ab7fe87bc5ae48a
Jedi Council <pitak3g.sc@gmail.com>
2016-12-24 20:23:16 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -2,4 +2,5 @@
 $proxy = 'velodrome.usefixie.com:80';
 $proxyauth = 'fixie:ncIwXPyhNi4cEWL';
 $access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
+$originalUrl = "https://pacific-scrubland-67443.herokuapp.com/";
 ?>
