2b82aace61fbe5a1ae60da5258d256193798358a
Jedi Council <pitak3g.sc@gmail.com>
2016-12-24 18:08:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,9 +1,5 @@
 <?php
-$proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:ncIwXPyhNi4cEWL';
-
-$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
-
+include 'headbot.php';
 // Function Return message
 function t1($tt1)
 {
