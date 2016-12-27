b0ed1c2059d868c6a8bf4f5df3b383cb91b1bafc
Jedi Council <pitak3g.sc@gmail.com>
2016-12-28 04:45:21 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -1,13 +1,13 @@
 <?php
 //Pitak SC
-//$proxy = 'velodrome.usefixie.com:80';
-//$proxyauth = 'fixie:ncIwXPyhNi4cEWL';
-//$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
-//$originalUrl = "https://pacific-scrubland-67443.herokuapp.com/";
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:ncIwXPyhNi4cEWL';
+$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
+$originalUrl = "https://pacific-scrubland-67443.herokuapp.com/";
 
 //Pitak MM
-$proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:9IBatB3cXnwXVOx';
-$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
-$originalUrl = "https://hidden-mountain-56713.herokuapp.com/";
+//$proxy = 'velodrome.usefixie.com:80';
+//$proxyauth = 'fixie:9IBatB3cXnwXVOx';
+//$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
+//$originalUrl = "https://hidden-mountain-56713.herokuapp.com/";
 ?>
