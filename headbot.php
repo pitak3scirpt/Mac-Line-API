59a35974f94e12b59a383b2714743d650d6b243b
Jedi Council <pitak3g.sc@gmail.com>
2016-12-30 09:14:06 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -8,7 +8,7 @@ $proxyauth = 'fixie:9IBatB3cXnwXVOx';
 
 $access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
 $originalUrl = "https://pacific-scrubland-67443.herokuapp.com/";
-$dataUrl = "https://hidden-mountain-56713.herokuapp.com/";
+$dataPrEqUrl = "https://hidden-mountain-56713.herokuapp.com/priorityeq/";
 //Test No security
 //$priorityEqUrl = "http://www.ppp066.16mb.com/priorityeq/";
 ?>
