8325825d4ba503df28eaafbd88eeb54dcec77cf6
Jedi Council <pitak3g.sc@gmail.com>
2017-01-02 21:02:06 +0700

    Update test_mail.php

--- test_mail.php
+++ test_mail.php
@@ -1,3 +1,10 @@
 <?php
 echo "test mail";
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+if (($event['headers']['Return-Path'] == 'pitak.m@egat.co.th') {
+  echo "Mail Recieve";
+}
 ?>
