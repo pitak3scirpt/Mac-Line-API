de5b4a5abe0646b5d9492e868f8c3e7f43fc32a7
Jedi Council <pitak3g.sc@gmail.com>
2017-05-03 14:19:10 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -12,7 +12,7 @@ $dataPrEqUrl = "https://hidden-mountain-56713.herokuapp.com/priorityeq/";
 $dataSubUrl = "https://hidden-mountain-56713.herokuapp.com/sub/";
 $datapkUrl = "https://hidden-mountain-56713.herokuapp.com/peak/";
 $ecsURL = "https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=6afd971f8d42fe9fcf578194fdf912af%2F%2FMac_Power.jpg";
-$ecsPreviewURL = "https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=6afd971f8d42fe9fcf578194fdf912af%2F%2FMac_PowerPreview.JPG";
+$ecsPreviewURL = "https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=6afd971f8d42fe9fcf578194fdf912af%2F%2FMac_PowerPreview.jpg";
 
 //$ecsURL = "https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=6afd971f8d42fe9fcf578194fdf912af%2F%2FMac_Power.jpg";
 //$ecsPreviewURL = "https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=6afd971f8d42fe9fcf578194fdf912af%2F%2FMac_Power.jpg";
