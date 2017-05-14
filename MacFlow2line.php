642b76b3b43660aea1f573a474bc91a4ac0fb1f8
Jedi Council <pitak3g.sc@gmail.com>
2017-05-14 13:13:31 +0700

    Update MacFlow2line.php

--- MacFlow2line.php
+++ MacFlow2line.php
@@ -13,7 +13,7 @@ function im1($originalContentUrl,$previewImageUrl)
 
 $originalContentUrl = $ecsURL;
 $previewImageUrl = $ecsPreviewURL;
-
+$text = $_GET["password"];
 $text = "password";
 
 if (!is_null($text)) {
