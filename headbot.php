52e5d62659d224c480dc75a24b64fe9870718d7e
Jedi Council <pitak3g.sc@gmail.com>
2017-05-14 13:26:37 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -1,10 +1,10 @@
 <?php
 //Pitak SC
-//$proxy = 'velodrome.usefixie.com:80';
-//$proxyauth = 'fixie:ncIwXPyhNi4cEWL';
-//Pitak MM
 $proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:9IBatB3cXnwXVOx';
+$proxyauth = 'fixie:ncIwXPyhNi4cEWL';
+//Pitak MM
+//$proxy = 'velodrome.usefixie.com:80';
+//$proxyauth = 'fixie:9IBatB3cXnwXVOx';
 
 $access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
 $originalUrl = "https://pacific-scrubland-67443.herokuapp.com/";
