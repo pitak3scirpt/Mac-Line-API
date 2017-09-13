da6753d492085f8b2dd3f7e95aa5523827f6d1b9
Jedi Council <pitak3g.sc@gmail.com>
2017-09-13 19:39:56 +0700

    Update Template2Line.php

--- Template2Line.php
+++ Template2Line.php
@@ -1,11 +1,23 @@
 <?php
 include 'headbot.php';
 // Function Return message
-function t1($tt1)
+function temp2op($tt1)
 {
+	$act1 = [
+		'type' => 'postback',
+		'lable' => 'Buy',
+		'data' => 'action=buy&itemid=123'	
+		];
 	$messages = [
-		'type' => 'text',
-		'text' => $tt1
+		'type' => 'template',
+		'altText' => 'this is a buttons template',
+		'template => [
+			'type' => 'buttons',
+			'thumbnailImageUrl' => 'https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=fd20b4335410e38c017713bd6d458deb%2F%2FMac_Power.jpg',
+			'title' => 'Menu',
+			'text' => 'Please Select',
+			'action' => [$act1,$act1]
+			]
 		];
 	return $messages;
 }
@@ -16,7 +28,7 @@ $text = "TestTest";
 if (!is_null($text)) {
 //if (!empty($_POST)){
 	//$text = "ได้รับ Mail จาก :".$return_path."\nหัวข้อ :".$subject."\nเนื่อหา".$plain;
-	$messages = t1($text);
+	$messages = temp2op($text);
 	$url = 'https://api.line.me/v2/bot/message/push';
 	$data = [
   		'to' => 'Cff6b78bc839c9f764ffff0f066606681',
