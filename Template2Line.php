0e7dbf4cdf97f3f86d8bce92d8aa45983e432e17
Jedi Council <pitak3g.sc@gmail.com>
2017-09-13 23:12:18 +0700

    Update Template2Line.php

--- Template2Line.php
+++ Template2Line.php
@@ -269,9 +269,73 @@ if (!is_null($text)) {
 //if (!empty($_POST)){
 	//$text = "ได้รับ Mail จาก :".$return_path."\nหัวข้อ :".$subject."\nเนื่อหา".$plain;
 	//$messages = t1($text);
-	//$messages = temp2butt($text);
+	$messages = temp2butt($text);
 	//$messages = temp2comf($text);
 	//$messages = temp2caro($text);
+	//$messages = temp2imgcol($text);
+	$url = 'https://api.line.me/v2/bot/message/push';
+	$data = [
+  		'to' => 'Cff6b78bc839c9f764ffff0f066606681',
+		'messages' => [$messages]
+		];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_PROXY, $proxy);
+	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result . "\r\n";
+	
+
+	$messages = temp2comf($text);
+	$url = 'https://api.line.me/v2/bot/message/push';
+	$data = [
+  		'to' => 'Cff6b78bc839c9f764ffff0f066606681',
+		'messages' => [$messages]
+		];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_PROXY, $proxy);
+	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result . "\r\n";
+	
+	$messages = temp2caro($text);
+	$url = 'https://api.line.me/v2/bot/message/push';
+	$data = [
+  		'to' => 'Cff6b78bc839c9f764ffff0f066606681',
+		'messages' => [$messages]
+		];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_PROXY, $proxy);
+	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result . "\r\n";
+	
 	$messages = temp2imgcol($text);
 	$url = 'https://api.line.me/v2/bot/message/push';
 	$data = [
